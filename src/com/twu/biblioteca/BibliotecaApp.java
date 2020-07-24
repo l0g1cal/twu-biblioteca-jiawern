e4d71e02ca0b99fe99b57948a77435efc619847e
Jia Wern <yongjiawern@gmail.com>
2020-07-24 19:46:06 +0800

    Add welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,10 @@
 package com.twu.biblioteca;
 
+import static com.twu.biblioteca.Constants.WELCOME_MESSAGE;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(WELCOME_MESSAGE);
     }
 }
---
+++ src/com/twu/biblioteca/Constants.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public final class Constants {
+
+    private Constants() {}
+
+    public static final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+}
