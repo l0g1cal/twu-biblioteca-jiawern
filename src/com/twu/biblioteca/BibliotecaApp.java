fc90a3707f9de41b8735865ffe8b1ff99224963e
Jia Wern <yongjiawern@gmail.com>
2020-07-27 22:10:37 +0800

    Add menu option, invalid option message and exit application option

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,38 +1,75 @@
 package com.twu.biblioteca;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-import static com.twu.biblioteca.Constants.WELCOME_MESSAGE;
+import static com.twu.biblioteca.Constants.*;
 
 public class BibliotecaApp {
     private static Library library;
-
-    private BibliotecaApp() {
-        library = createNewLibrary();
-    }
+    private static Map<Integer, Option> optionMap = new HashMap<>();
+    private static Map<Integer, String> optionDescMap = new HashMap<>();
 
     public static void main(String[] args) {
         createNewBiblioteca();
         displayWelcomeMessage();
-        displayAllBooks();
+
+        while (true) {
+            awaitUserSelection();
+        }
     }
 
     private static BibliotecaApp createNewBiblioteca() {
         return new BibliotecaApp();
     }
 
+    private BibliotecaApp() {
+        createNewLibrary();
+        createUserOptions();
+    }
+
     private static void displayWelcomeMessage() {
-        System.out.println(WELCOME_MESSAGE);
+        printToCommandLine(WELCOME_MESSAGE);
     }
 
-    private static void displayAllBooks() {
-        String booksString = library.pprintBooks();
-        System.out.println("Here is the list of books in this Biblioteca");
-        System.out.println(booksString);
+    private static void awaitUserSelection() {
+        printToCommandLine("Please select an option number to continue ... Or type 'exit' to end program\n");
+        displayOptions();
+        String line = getUserInput();
+        verifyAndRunInput(line);
     }
 
-    private static Library createNewLibrary() {
-        return new Library(createNewBooks());
+    private static void displayOptions() {
+        for (int optionNumber = 1; optionNumber <= optionMap.size(); optionNumber++) {
+            String outputString = String.format("%d: %s", optionNumber, optionDescMap.get(optionNumber));
+            printToCommandLine(outputString);
+        }
+    }
+
+    private static String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static void verifyAndRunInput(String ipt) {
+        if (ipt.equals("exit")) {
+            printToCommandLine("User requested to end program. Exiting ...");
+            System.exit(0);
+        }
+        try {
+            Integer iptValue = Integer.parseInt(ipt);
+            Option opt = optionMap.get(iptValue);
+            opt.run();
+        }
+        catch (Exception e) {
+            printToCommandLine("Please select a valid option!");
+        }
+    }
+
+    private static void createNewLibrary() {
+        library = new Library(createNewBooks());
     }
 
     private static ArrayList<Book> createNewBooks() {
@@ -44,4 +81,21 @@ public class BibliotecaApp {
         return books;
     }
 
+    private static void createUserOptions() {
+        optionMap.put(DISPLAY_BOOKS_OPTION, new DisplayBooksOption());
+        optionDescMap.put(DISPLAY_BOOKS_OPTION, DISPLAY_BOOKS_OPTION_DESC);
+    }
+
+    private static class DisplayBooksOption implements Option {
+        @Override
+        public void run() {
+            String booksString = library.pprintBooks();
+            printToCommandLine("Here is the list of books in this Biblioteca");
+            printToCommandLine(booksString);
+        }
+    }
+
+    private static void printToCommandLine(String message) {
+        System.out.println(message);
+    }
 }
--- src/com/twu/biblioteca/Constants.java
+++ src/com/twu/biblioteca/Constants.java
@@ -5,4 +5,7 @@ public final class Constants {
     private Constants() {}
 
     public static final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
+    public static final Integer DISPLAY_BOOKS_OPTION = 1;
+    public static final String DISPLAY_BOOKS_OPTION_DESC = "List of books";
 }
