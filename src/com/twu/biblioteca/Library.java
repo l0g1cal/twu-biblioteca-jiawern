3081ac849bdd3375ad03eccea8e611fec519887c
Jia Wern <yongjiawern@gmail.com>
2020-07-28 18:51:15 +0800

    Add checkoutbook functionality

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -3,10 +3,26 @@ package com.twu.biblioteca;
 import java.util.ArrayList;
 
 public class Library {
-    private ArrayList<Book> books;
+    private ArrayList<Book> books;;
+    private ArrayList<Book> borrowedBooks = new ArrayList<Book>();;
 
     public Library(ArrayList<Book> books) {
         this.books = books;
+    }
+
+    public int totalBooksInLibrary() {
+        return books.size();
+    }
+
+    public String checkoutBook(Book book) {
+        for (Book b : books) {
+            if (b.equals(book)) {
+                books.remove(b);
+                borrowedBooks.add(b);
+                return "Thank you! Enjoy the book";
+            }
+        }
+        return "Book is unavailable";
     }
 
     public String pprintBooks() {
