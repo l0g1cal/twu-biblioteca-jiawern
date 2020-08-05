817604346aad706b8f71442cc4e59b8669dda8cc
Jia Wern <yongjiawern@gmail.com>
2020-08-05 23:38:19 +0800

    Add more test cases for library methods

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -22,6 +22,8 @@ public class LibraryTest {
     @Test
     public void checkoutBookWhenBookAvailableShouldReturnTrueAndMakeBookUnavailable() {
         assertEquals(1, library.totalBooksInLibrary());
+        assertEquals(0, library.totalBorrowedBooks());
+
         Book bookToCheckout = books.get(0);
         boolean output = library.checkoutBook(bookToCheckout);
         assertEquals(true, output);
@@ -30,12 +32,50 @@ public class LibraryTest {
     }
 
     @Test
-    public void returnBookWhenBookBelongsToLibraryShouldReturnTrueAndMakeBookAvailable() {
+    public void checkoutBookWhenBookUnavailableShouldReturnFalse() {
         assertEquals(1, library.totalBooksInLibrary());
+        assertEquals(0, library.totalBorrowedBooks());
+
         Book bookToCheckout = books.get(0);
         library.checkoutBook(bookToCheckout);
+        assertEquals(0, library.totalBooksInLibrary());
+        assertEquals(1, library.totalBorrowedBooks());
+
+        boolean output_unavailable = library.checkoutBook(bookToCheckout);
+        assertEquals(false, output_unavailable);
+        assertEquals(0, library.totalBooksInLibrary());
+        assertEquals(1, library.totalBorrowedBooks());
+    }
+
+    @Test
+    public void returnBookWhenBookBelongsToLibraryShouldReturnTrueAndMakeBookAvailable() {
+        assertEquals(1, library.totalBooksInLibrary());
+        assertEquals(0, library.totalBorrowedBooks());
+
+        Book bookToCheckout = books.get(0);
+        library.checkoutBook(bookToCheckout);
+        assertEquals(0, library.totalBooksInLibrary());
+        assertEquals(1, library.totalBorrowedBooks());
+
         boolean returnOutput = library.returnBook(bookToCheckout);
         assertEquals(true, returnOutput);
+        assertEquals(1, library.totalBooksInLibrary());
+        assertEquals(0, library.totalBorrowedBooks());
+    }
+
+    @Test
+    public void returnBookWhenBookDoesNotBelongToLibraryShouldReturnFalse() {
+        assertEquals(1, library.totalBooksInLibrary());
+        assertEquals(0, library.totalBorrowedBooks());
+
+        Book bookToCheckout = books.get(0);
+        library.checkoutBook(bookToCheckout);
+        library.returnBook(bookToCheckout);
+        assertEquals(1, library.totalBooksInLibrary());
+        assertEquals(0, library.totalBorrowedBooks());
+
+        boolean returnOutput = library.returnBook(bookToCheckout);
+        assertEquals(false, returnOutput);
         assertEquals(1, library.totalBooksInLibrary());
         assertEquals(0, library.totalBorrowedBooks());
     }
