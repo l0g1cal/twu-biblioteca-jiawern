7dd3a6de99e0a7d8eff01aed831bd9db3a8dbce7
Jia Wern <yongjiawern@gmail.com>
2020-08-09 17:55:46 +0800

    Add test for DisplayMoviesOption

---
+++ test/com/twu/biblioteca/OptionTests/DisplayMoviesOptionTest.java
@@ -0,0 +1,57 @@
+package com.twu.biblioteca.OptionTests;
+
+import com.twu.biblioteca.Book;
+import com.twu.biblioteca.Library;
+import com.twu.biblioteca.Movie;
+import com.twu.biblioteca.option.DisplayBooksOption;
+import com.twu.biblioteca.option.Option;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class DisplayMoviesOptionTest {
+    private ArrayList<Book> books = new ArrayList<Book>();
+    private ArrayList<Movie> movies = new ArrayList<Movie>();
+    private Library library;
+    private Movie movie1;
+    private Movie movie2;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Before
+    public void setUp() {
+        movie1 = new Movie("title1","2020", "director1");
+        movie2 = new Movie("title2","2020", "director2", 9);
+        movies.add(movie1);
+        movies.add(movie2);
+
+        library = new Library(books, movies);
+    }
+
+    @Test
+    public void displayMoviesOptionWhenRunShouldReturnListOfMovies() {
+        Option displayOption = new DisplayMoviesOption();
+        displayOption.run(library, null);
+        String expectedOutput = "Here is the list of movies in this Biblioteca\n" +
+                "0) title1,2020,director1\n" +
+                "1) title2,2020,director2,9\n";
+        String actualOutput = outContent.toString().trim() + "\n";
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+}
