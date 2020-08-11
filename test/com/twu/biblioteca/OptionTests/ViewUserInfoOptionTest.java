7d2cee12ffa12bd73abe530b412d5788436376f2
Jia Wern <yongjiawern@gmail.com>
2020-08-11 17:10:19 +0800

    Add ViewUserInfoOptionTest

---
+++ test/com/twu/biblioteca/OptionTests/ViewUserInfoOptionTest.java
@@ -0,0 +1,44 @@
+package com.twu.biblioteca.OptionTests;
+
+import com.twu.biblioteca.BibliotecaApp;
+import com.twu.biblioteca.User;
+import com.twu.biblioteca.option.Option;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class ViewUserInfoOptionTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Before
+    public void setUp() {
+        User user1 = new User("user1", "123-45678", "p1", "user1@gmail.com", "12345678", true);
+        BibliotecaApp.setCurrentUser(user1);
+    }
+
+    @Test
+    public void displayMoviesOptionWhenRunShouldReturnListOfMovies() {
+        Option viewUserInfoOption = new ViewUserInfoOption();
+        viewUserInfoOption.run(null, null, null);
+        String expectedOutput = "Here is your information\n" +
+                "user1,user1@gmail.com,12345678\n";
+        String actualOutput = outContent.toString().trim() + "\n";
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+}
